e29e68b435429ccc87b91a893318704fbf4faa86
albin <md.albin.hossain@icloud.com>
2023-07-02 12:28:44 +0600

    update

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -1,38 +1,26 @@
 export async function onRequestPost(context) {
-  // console.log(env.DKIM_PRIVATE_KEY);
+  let input = await context.request.formData();
   let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
     method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
+    headers: { "content-type": "application/json", },
     body: JSON.stringify({
-      personalizations: [
-        {
-          to: [
-            {
-              email: "to@albin.com.bd", // add your to email here
-            },
-          ],
-          dkim_domain: "albin.com.bd", // add your domain
-          dkim_selector: "mailchannels",
-          dkim_private_key: context.env.DKIM_PRIVATE_KEY,
-        },
-      ],
-      from: {
-        email: "from@albin.com.bd", // add your from email here
-      },
-      subject: "Subject",
-      content: [
-        {
-          type: "text/plain",
-          value: "message",
-        },
-      ],
+      personalizations: [{
+        to: [{ email: "contact@albin.com.bd", name: "Contact Form" },],
+        dkim_domain: "albin.com.bd",
+        dkim_selector: "mailchannels",
+        dkim_private_key: context.env.DKIM_PRIVATE_KEY,
+      },],
+      from: { email: "contact@albin.com.bd", name: "Contact Form" },
+      reply_to: { email: input.get("email") },
+      subject: input.get("subject"),
+      content: [{
+        type: "text/plain",
+        value: input.get("message"),
+      },],
     }),
   });
 
   let respContent = "";
-  // only send the mail on "POST", to avoid spiders, etc.
   if (context.request.method == "POST") {
     const resp = await fetch(send_request);
     const respText = await resp.text();
@@ -41,7 +29,5 @@ export async function onRequestPost(context) {
   }
 
   let htmlContent = `<html><head></head><body><p>Click to send message: <form method="post"><input type="submit" value="Send"/></form></p><pre>${respContent}</pre></body></html>`;
-  return new Response(htmlContent, {
-    headers: { "content-type": "text/html" },
-  });
+  return new Response(htmlContent, { headers: { "content-type": "text/html" }, });
 }
