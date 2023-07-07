be33f1920c006e80f7b38ead4d97e402bd48cd26
albin <md.albin.hossain@icloud.com>
2023-07-07 22:05:35 +0600

    fix bug

--- functions/submit.ts
+++ functions/submit.ts
@@ -15,8 +15,8 @@ export async function onRequestPost(context) {
         dkim_selector: "mailchannels",
         dkim_private_key: context.env.DKIM_PRIVATE_KEY,
       },],
-      from: { email: input.get("email") ?? "anonymous@albin.com.bd", name: input.get("name") ?? "Anonymous" },
-      reply_to: { email: input.get("email") ?? "anonymous@albin.com.bd", name: input.get("name") ?? "Anonymous" },
+      from: { email: input.get("from") ?? "anonymous@albin.com.bd", name: input.get("name") ?? "Anonymous" },
+      reply_to: { email: (input.get("from") ?? input.get("email")) ?? "anonymous@albin.com.bd", name: input.get("name") ?? "Anonymous" },
       subject: input.get("subject") ?? "Anonymous",
       content: [{
         type: "text/html",
