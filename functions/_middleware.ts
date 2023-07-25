c7506bfa0bf5dc5c0765e47956904f210a315666
albin <md.albin.hossain@icloud.com>
2023-07-26 00:01:09 +0600

    update

---
+++ functions/_middleware.ts
@@ -0,0 +1,21 @@
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest: PagesFunction = (context) => mailChannelsPlugin({
+    personalizations: [
+        {
+            to: [{ name: "Albin", email: "mail@albin.com.bd" }],
+            "dkim_domain": "albin.com.bd",
+            "dkim_selector": "mailchannels",
+            "dkim_private_key": context.env.DKIM_PRIVATE_KEY
+        },
+    ],
+    from: {
+        name: "Contact Form",
+        email: "contact@albin.com.bd",
+    },
+    respondWith: () => {
+        return new Response(
+            `Thank you for submitting.`
+        );
+    },
+})(context);
