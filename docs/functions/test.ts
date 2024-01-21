4b9c7d58e42f8cad41c3b75c822ac1fc504fd9f2
albin <md.albin.hossain@icloud.com>
2024-01-21 18:49:25 +0600

    update

---
+++ docs/functions/test.ts
@@ -0,0 +1,4 @@
+export async function onRequestPost(context) {  
+    let htmlContent = `<html><head></head><body><h1 style="text-align:center; margin-top:40px">Hello World</h1></body></html>`;
+    return new Response(htmlContent, { headers: { "content-type": "text/html" }, });
+  }
