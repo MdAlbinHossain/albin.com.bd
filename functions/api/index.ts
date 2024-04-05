855284dc9e69866c414a0fc95ae7b46a1cde69ec
Albin <mail@albin.com.bd>
2024-04-05 15:47:33 +0600

    Add configuration files and dependencies

--- functions/api/index.ts
+++ functions/api/index.ts
@@ -1,8 +1,13 @@
-// interface Env {
-// 	KV: KVNamespace;
-// }
+export interface Env {
+	// MY_KV_NAMESPACE: KVNamespace;
+	// MY_DB: D1Database;
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	// MY_BUCKET: R2Bucket;
+	// MY_SERVICE: Fetcher;
+	// MY_QUEUE: Queue;
+}
 
-export const onRequest: PagesFunction = async (context) => {
+export const onRequest: PagesFunction<Env> = async (context) => {
 	// const value = await context.env.KV.get('example');
  	return new Response("Hello World");
 }
