9be2fe7e03f7ad48c0ca605853855df2badb612c
Albin <mail@albin.com.bd>
2024-04-07 15:57:03 +0600

    Update allowedOrigins in API endpoint to include 'https://dev.mdalbinhossain.pages.dev'

--- functions/api/forms/index.ts
+++ functions/api/forms/index.ts
@@ -37,7 +37,7 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
 	}
 
 	const requestOrigin = request.headers.get('Origin');
-	const allowedOrigins = ['https://albin.com.bd', 'https://mdalbinhossain.pages.dev'];
+	const allowedOrigins = ['https://albin.com.bd', 'https://mdalbinhossain.pages.dev', 'https://dev.mdalbinhossain.pages.dev'];
 
 	if (requestOrigin && allowedOrigins.includes(requestOrigin)) {
 		const reqHeaders = await readRequestHeaders(request);
