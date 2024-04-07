bcf1b01e78216d18d5f51be012c0cb13531305d6
Albin <mail@albin.com.bd>
2024-04-07 11:52:20 +0600

    Refactor API function to handle user requests and update database bindings

--- functions/api/index.ts
+++ functions/api/index.ts
@@ -1,15 +1,15 @@
-export interface Env {
-	// MY_KV_NAMESPACE: KVNamespace;
-	// MY_DB: D1Database;
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	// MY_BUCKET: R2Bucket;
-	// MY_SERVICE: Fetcher;
-	// MY_QUEUE: Queue;
+import { Hono } from 'hono'
+
+type Bindings = {
+  DB: D1Database
 }
 
-export const onRequest: PagesFunction<Env> = async (context) => {
-	return new Response(JSON.stringify({
-		headers: Object.fromEntries(context.request.headers),
-	}),
-		{ headers: { 'content-type': 'application/json' } });
-}
+const app = new Hono<{ Bindings: Bindings }>()
+
+
+app.get("/:id", async (c) => {
+  const userId= c.req.param("id")
+  return c.json({ userId})
+})
+
+export default app
