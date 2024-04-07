7af0ecfb90f3f94fa8c7b0fc8d86dad5b201b2f9
Albin <mail@albin.com.bd>
2024-04-07 15:41:44 +0600

    Refactor API endpoint to use "/api/forms/" for user requests and update database bindings

--- functions/api/forms/index.ts
+++ functions/api/forms/index.ts
@@ -1,10 +1,7 @@
 export interface Env {
-	// MY_KV_NAMESPACE: KVNamespace;
+	KV: KVNamespace;
 	DB: D1Database;
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
 	// MY_BUCKET: R2Bucket;
-	// MY_SERVICE: Fetcher;
-	// MY_QUEUE: Queue;
 }
 
 type FormResponse = {
