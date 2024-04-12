fa103ee53397c2e665a5e21d5a64f8f7981a6dc1
Albin <mail@albin.com.bd>
2024-04-12 18:12:53 +0600

    Remove LinkedIn API endpoint and related files

---
+++ functions/github/index.ts
@@ -0,0 +1,3 @@
+export const onRequestGet: PagesFunction = async (context) => {
+    return Response.redirect('https://github.com/MdAlbinHossain/', 301);
+}
--- pages/others/github.md
+++
@@ -1,8 +0,0 @@
----
-sitemap: false
-permalink: /github/
-redirect_to: https://github.com/MdAlbinHossain
-redirect_from:
-    - /Github
-    - /GitHUb
----
